c9e330e822654eb14d279a852c9fee87c8c18ec8
Cesar Benavides <cesarbenavides@icloud.com>
2021-01-11 15:37:28 -0600

    Hopefully pushing release cantidate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './styles/reset.scss';
 
-
 export * from './components/Badge';
 export * from './components/Button';
-export * from './components/Typography';
 export * from './components/Icon';
+export * from './components/Typography';
