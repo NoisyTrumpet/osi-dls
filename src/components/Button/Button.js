326af0f19373e97ff616955366ab74e21dbd053b
Cesar Benavides <cesarbenavides@icloud.com>
2021-01-13 09:39:17 -0600

    feat: Animated Logo & Typography fonts fix

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,153 +1,156 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Button.scss';
-import classNames from 'classnames';
-import { Typography } from '../Typography';
-import { Icon } from '../Icon';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Button.scss";
+import classNames from "classnames";
+import { Typography } from "../Typography";
+import { Icon } from "../Icon";
 
-const Button = React.forwardRef(({
-  type,
-  mode,
-  disabled,
-  onBlur,
-  onFocus,
-  color,
-  children,
-  iconLeft,
-  iconRight,
-  click,
-  addClass,
-  typoVariant,
-  ariaControls,
-  ariaSelected,
-  role,
-  id,
-  onKeyDown,
-  onKeyUp,
-  tabIndex,
-  testId,
-}, ref) => {
-  const classes = classNames(
-    'btn',
-    `btn-${mode}`,
-    (iconLeft || iconRight) && 'has-icon',
+const Button = React.forwardRef(
+  (
     {
-      'has-icon-left': iconLeft,
-      'has-icon-right': iconRight,
+      type,
+      mode,
+      disabled,
+      onBlur,
+      onFocus,
+      color,
+      children,
+      iconLeft,
+      iconRight,
+      click,
+      addClass,
+      typoVariant,
+      ariaControls,
+      ariaSelected,
+      role,
+      id,
+      onKeyDown,
+      onKeyUp,
+      tabIndex,
+      testId
     },
-    addClass,
-  );
+    ref
+  ) => {
+    const classes = classNames(
+      "btn",
+      `btn-${mode}`,
+      (iconLeft || iconRight) && "has-icon",
+      {
+        "has-icon-left": iconLeft,
+        "has-icon-right": iconRight
+      },
+      addClass
+    );
+    const btnDetails = {
+      primary: {
+        iconSize: 20,
+        color: color || "brand-white",
+        typoVariant: typoVariant || "cta"
+      },
+      secondary: {
+        iconSize: 20,
+        color: color || "primary",
+        typoVariant: typoVariant || "cta"
+      },
+      tertiary: {
+        iconSize: 10,
+        color: color || "primary",
+        typoVariant: typoVariant || "cta-tertiary"
+      },
+      "quick-action-deactivated": {
+        iconSize: 10,
+        color: color || "primary",
+        typoVariant: typoVariant || "cta-tertiary"
+      },
+      "quick-action-activated": {
+        iconSize: 10,
+        color: color || "primary",
+        typoVariant: typoVariant || "cta-tertiary"
+      },
+      "text-link": {
+        iconSize: 14,
+        color: color || "accentB",
+        typoVariant: typoVariant || "text-link"
+      },
+      "text-link-2": {
+        iconSize: 12,
+        color: color || "primary",
+        typoVariant: typoVariant || "caption"
+      },
+      custom: {
+        iconSize: 20,
+        color: color || "brand-black",
+        typoVariant: typoVariant || "body"
+      }
+    };
 
-  const btnDetails = {
-    primary: {
-      iconSize: 20,
-      color: color || 'brand-white',
-      typoVariant: typoVariant || 'cta',
-    },
-    secondary: {
-      iconSize: 20,
-      color: color || 'primary',
-      typoVariant: typoVariant || 'cta',
-    },
-    tertiary: {
-      iconSize: 10,
-      color: color || 'primary',
-      typoVariant: typoVariant || 'cta-tertiary',
-    },
-    'quick-action-deactivated': {
-      iconSize: 10,
-      color: color || 'primary',
-      typoVariant: typoVariant || 'cta-tertiary',
-    },
-    'quick-action-activated': {
-      iconSize: 10,
-      color: color || 'primary',
-      typoVariant: typoVariant || 'cta-tertiary',
-    },
-    'text-link': {
-      iconSize: 14,
-      color: color || 'accentB',
-      typoVariant: typoVariant || 'text-link',
-    },
-    'text-link-2': {
-      iconSize: 12,
-      color: color || 'primary',
-      typoVariant: typoVariant || 'caption',
-    },
-    custom: {
-      iconSize: 20,
-      color: color || 'brand-black',
-      typoVariant: typoVariant || 'body',
-    },
-  };
-
-  return (
-    // eslint-disable-next-line react/button-has-type
-    <button
-      type={type}
-      onClick={click}
-      className={classes}
-      disabled={disabled}
-      ref={ref}
-      onBlur={onBlur}
-      onFocus={onFocus}
-      aria-controls={ariaControls}
-      aria-selected={ariaSelected}
-      role={role}
-      id={id}
-      data-testid={testId}
-      tabIndex={tabIndex}
-      onKeyDown={onKeyDown}
-      onKeyUp={onKeyUp}
-    >
-      
-      {iconLeft && (
-        <Icon
-          name={iconLeft}
-          color={btnDetails[mode].color}
-          size={btnDetails[mode].iconSize}
-          disabled={disabled}
-          addClass="icon-left"
-        />
-      )}
-      <Typography
-        variant={btnDetails[mode].typoVariant}
-        color={btnDetails[mode].color}
+    return (
+      // eslint-disable-next-line react/button-has-type
+      <button
+        type={type}
+        onClick={click}
+        className={classes}
+        disabled={disabled}
+        ref={ref}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        aria-controls={ariaControls}
+        aria-selected={ariaSelected}
+        role={role}
+        id={id}
+        data-testid={testId}
+        tabIndex={tabIndex}
+        onKeyDown={onKeyDown}
+        onKeyUp={onKeyUp}
       >
-        {children}
-      </Typography>
-      {iconRight && (
-        <Icon
-          name={iconRight}
+        {iconLeft && (
+          <Icon
+            name={iconLeft}
+            color={btnDetails[mode].color}
+            size={btnDetails[mode].iconSize}
+            disabled={disabled}
+            addClass="icon-left"
+          />
+        )}
+        <Typography
+          variant={btnDetails[mode].typoVariant}
           color={btnDetails[mode].color}
-          size={btnDetails[mode].iconSize}
-          disabled={disabled}
-          addClass="icon-right"
-        />
-      )}
-    </button>
-  );
-});
+        >
+          {children}
+        </Typography>
+        {iconRight && (
+          <Icon
+            name={iconRight}
+            color={btnDetails[mode].color}
+            size={btnDetails[mode].iconSize}
+            disabled={disabled}
+            addClass="icon-right"
+          />
+        )}
+      </button>
+    );
+  }
+);
 
 Button.defaultProps = {
-  type: 'button',
-  mode: 'primary',
+  type: "button",
+  mode: "primary",
   disabled: false,
-  color: '',
-  iconLeft: '',
-  iconRight: '',
+  color: "",
+  iconLeft: "",
+  iconRight: ""
 };
 
 Button.propTypes = {
   mode: PropTypes.oneOf([
-    'primary',
-    'secondary',
-    'tertiary',
-    'quick-action-deactivated',
-    'quick-action-activated',
-    'text-link',
-    'text-link-2',
-    'custom',
+    "primary",
+    "secondary",
+    "tertiary",
+    "quick-action-deactivated",
+    "quick-action-activated",
+    "text-link",
+    "text-link-2",
+    "custom"
   ]),
   disabled: PropTypes.bool,
   onBlur: PropTypes.func,
@@ -159,36 +162,32 @@ Button.propTypes = {
   click: PropTypes.func,
   addClass: PropTypes.string,
   typoVariant: PropTypes.oneOf([
-    'headline-1',
-    'headline-2',
-    'headline-3',
-    'headline-4',
-    'headline-5',
-    'headline-6',
-    'price-unit',
-    'price-unit-strikethrough',
-    'price-sale',
-    'cta',
-    'cta-tertiary',
-    'subtitle',
-    'field-label',
-    'body',
-    'body-medium',
-    'eyebrow',
-    'text-link',
-    'input-text',
-    'caption',
-    'helper',
-    'breadcrumb',
-    'srt',
-    'licensing',
-    'subtext',
+    "headline-1",
+    "headline-2",
+    "headline-3",
+    "headline-4",
+    "headline-5",
+    "headline-6",
+    "price-unit",
+    "price-unit-strikethrough",
+    "price-sale",
+    "cta",
+    "cta-tertiary",
+    "subtitle",
+    "field-label",
+    "body",
+    "body-medium",
+    "eyebrow",
+    "text-link",
+    "input-text",
+    "caption",
+    "helper",
+    "breadcrumb",
+    "srt",
+    "licensing",
+    "subtext"
   ]),
-  type: PropTypes.oneOf([
-    'button',
-    'submit',
-    'reset',
-  ]),
+  type: PropTypes.oneOf(["button", "submit", "reset"]),
   ariaControls: PropTypes.string,
   ariaSelected: PropTypes.string,
   tabIndex: PropTypes.string,
@@ -196,7 +195,7 @@ Button.propTypes = {
   onKeyDown: PropTypes.func,
   onKeyUp: PropTypes.func,
   id: PropTypes.string,
-  testId: PropTypes.string,
+  testId: PropTypes.string
 };
 
 export default Button;
