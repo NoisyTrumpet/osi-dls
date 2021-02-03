8722394de569cead6fc3dfd2c53c47148f25dacd
Cesar Benavides <cesarbenavides@icloud.com>
2021-02-02 18:02:47 -0600

    feat: style guide and removed wrapper from shell

--- src/ContentComponents/Shell/Shell.js
+++ src/ContentComponents/Shell/Shell.js
@@ -3,19 +3,17 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 import { Nav } from "../../components/Nav";
 import { Footer } from "../../components/Footer";
-import { Wrapper } from "../../components/Wrapper";
-
 import "./Shell.scss";
-// @TODO: Fix release
+
 const Shell = ({ children, contained }) => {
   const classes = classNames(contained ? "shell-contained" : "shell");
 
   return (
-    <Wrapper addClass={classes}>
+    <div className={classes}>
       <Nav />
       {children}
       <Footer />
-    </Wrapper>
+    </div>
   );
 };
 
