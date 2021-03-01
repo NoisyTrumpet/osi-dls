3f45d4f8de0cf7486ccd11b6a0686988145adc35
Cesar Benavides <cesarbenavides@icloud.com>
2021-03-01 13:17:05 -0600

    feat: speed fixes

--- src/components/Dialog/HoC/DialogManager.js
+++ src/components/Dialog/HoC/DialogManager.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 // Higher Order Component - https://reactjs.org/docs/higher-order-components.html
 const DialogManager = (WrappedModal, Context) => ({ children }) => {
   const [isOpen, setIsOpen] = React.useState(false);
   const [openedBy, setOpenedBy] = React.useState(null); // Refocuses on the trigger that opened the Dialog
 
-  const open = (e) => {
+  const open = e => {
     setIsOpen(true);
     if (e && e.currentTarget) {
       setOpenedBy(e.currentTarget);
