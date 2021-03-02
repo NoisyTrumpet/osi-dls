d71b7040e7ecfab321e81bff296556875b7903ce
Cesar Benavides <cesarbenavides@icloud.com>
2021-03-02 13:16:58 -0600

    feat: removing logo and fixing build hopefully

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -2,34 +2,6 @@ import AccentCircle from "./accent-circle.svg";
 import DesktopModalCurveBottom from "./desktop-modal-curve-bottom.svg";
 import DesktopModalCurveTop from "./desktop-modal-curve-top.svg";
 import LogoHoriz from "./logo_horiz.svg";
-import LogoHorizBlack from "./logo-horiz_black.svg";
-import LogoHorizWhite from "./logo-horiz_white.svg";
-import LogoHorizBlue from "./logo-horiz_blue.svg";
-import LogoHorizOrange from "./logo-horiz_orange.svg";
-
-import LogoRegular from "./logo-regular.svg";
-import LogoRegularBlack from "./logo_black.svg";
-import LogoRegularWhite from "./logo_white.svg";
-import LogoRegularBlue from "./logo_blue.svg";
-import LogoRegularOrange from "./logo_orange.svg";
-
-import LogoMark from "./logo_mark.svg";
-import LogoMarkBlack from "./mark_black.svg";
-import LogoMarkWhite from "./mark_white.svg";
-import LogoMarkBlue from "./mark_blue.svg";
-import LogoMarkOrange from "./mark_orange.svg";
-
-import LogoHorizTagline from "./logo-horiz-tagline.svg";
-import LogoHorizTaglineBlack from "./logo-horiz-tagline-black.svg";
-import LogoHorizTaglineWhite from "./logo-horiz-tagline-white.svg";
-import LogoHorizTaglineBlue from "./logo-horiz-tagline-blue.svg";
-import LogoHorizTaglineOrange from "./logo-horiz-tagline-orange.svg";
-
-import LogoTagline from "./logo-tagline.svg";
-import LogoTaglineBlack from "./logo-tagline-black.svg";
-import LogoTaglineWhite from "./logo-tagline-white.svg";
-import LogoTaglineBlue from "./logo-tagline-blue.svg";
-import LogoTaglineOrange from "./logo-tagline-orange.svg";
 import InfoGraph from "./infographic.svg";
 import MobileModalCurveTop from "./mobile-modal-curve-top.svg";
 import TabBrushStroke from "./tabs-brush-stroke.svg";
@@ -41,30 +13,6 @@ export {
   DesktopModalCurveTop,
   InfoGraph,
   LogoHoriz,
-  LogoHorizBlack,
-  LogoHorizWhite,
-  LogoHorizBlue,
-  LogoHorizOrange,
-  LogoRegular,
-  LogoRegularBlack,
-  LogoRegularWhite,
-  LogoRegularBlue,
-  LogoRegularOrange,
-  LogoMark,
-  LogoMarkBlack,
-  LogoMarkWhite,
-  LogoMarkBlue,
-  LogoMarkOrange,
-  LogoHorizTagline,
-  LogoHorizTaglineBlack,
-  LogoHorizTaglineWhite,
-  LogoHorizTaglineBlue,
-  LogoHorizTaglineOrange,
-  LogoTagline,
-  LogoTaglineBlack,
-  LogoTaglineWhite,
-  LogoTaglineBlue,
-  LogoTaglineOrange,
   MobileModalCurveTop,
   TabBrushStroke,
   WaveSVG
